7aef110c2134f0a0338f780923f1c786b8fbce00
Serge Najimov <najimovserge@gmail.com>
2022-12-12 23:47:01 +0100

    changed User Modal

---
+++ api/Data/Migrations/20221212222535_UserPasswordAdded.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserPasswordAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "Users",
+                type: "varbinary(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "Users");
+        }
+    }
+}
